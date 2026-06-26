a271a617a2b6713db95313f93b1aa2a0cb873283
Good Boy <goodboy@Goodboy-MacBook-Pro.local>
2026-06-26 13:56:19 -0400

    Initial commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///Users/goodboy/Documents/Crazyyyyyyyy/micro/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/goodboy/Documents/Crazyyyyyyyy/micro/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
